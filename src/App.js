71171671563b04656edf3c31f8cb530539609b02
AhmedYehiaa <ahmedyehia93@gmail.com>
2018-11-26 01:21:52 +0200

    add navbar to routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from './components/movies';
+import MovieForm from './components/movieForm';
 import NotFound from './components/notFound';
 import Rentals from './components/rentals';
 import Customers from './components/customer';
+import NavBar from './components/navBar';
 class App extends Component {
   render() {
     return (
-      <main role="main" className="container">
-        <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to='/not-found' />
-        </Switch>
-
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main role="main" className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to='/not-found' />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
