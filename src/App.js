8c153d0196a5e45124e1259a75d70cdf1c59808b
AhmedYehiaa <ahmedyehia93@gmail.com>
2018-11-25 14:05:36 +0200

    add Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from './components/movies';
+import NotFound from './components/notFound';
+import Rentals from './components/rentals';
+import Customers from './components/customer';
 class App extends Component {
   render() {
     return (
       <main role="main" className="container">
-          <Movies />
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to='/not-found' />
+        </Switch>
+
       </main>
     );
   }
---
+++ src/components/rentals.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Rentals = () => {
+  return (
+    <h1>Movie Form</h1>
+  );
+}
+
+export default Rentals;
